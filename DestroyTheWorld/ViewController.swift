2154c83c0d4c5544a8345bf8fa4e11d0d49d421f
H-47 <hts4224@gmail.com>
2016-12-14 12:05:52 +0300

    made a poinltess change
    for no valid reason whatsoever

--- DestroyTheWorld/ViewController.swift
+++ DestroyTheWorld/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        print("Hellow MFKN WORLD ;) ")
+        print("Hellow World ;) ")
     }
 
     override func didReceiveMemoryWarning() {
